d0fb4d5576614eca09b20ec3be910f9109214c0c
Christoffer Ljung <christoffer@ljung.io>
2024-12-08 20:23:12 +0100

    Add terminal keybindings

--- init.lua
+++ init.lua
@@ -8,7 +8,7 @@ vim.g.maplocalleader = ' '
 vim.g.have_nerd_font = true
 
 -- Set default shell
-vim.opt.shell = 'zsh'
+-- vim.opt.shell = 'zsh'
 vim.opt.guifont = 'JetBrainsMono Nerd Font:h12'
 
 -- [[ Setting options ]]
@@ -100,11 +100,17 @@ function python_shebang()
 end
 
 -- Custom keybindings
--- Resize split to the left (Ctrl + Shift + Left Arrow)
+-- Resize split (Ctrl + Shift + Right Arrow)
 vim.api.nvim_set_keymap('n', '<A-Left>', ':vertical resize -5<CR>', { noremap = true, silent = true })
--- Resize split to the right (Ctrl + Shift + Right Arrow)
 vim.api.nvim_set_keymap('n', '<A-Right>', ':vertical resize +5<CR>', { noremap = true, silent = true })
-
+vim.api.nvim_set_keymap('n', '<A-Up>', ':horizontal resize +5<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<A-Down>', ':horizontal resize -5<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('t', '<A-Up>', '<C-\\><C-n>:horizontal resize +5<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('t', '<A-Down>', '<C-\\><C-n>:horizontal resize -5<CR>', { noremap = true, silent = true })
+-- Exit terminal mode with ESC
+vim.api.nvim_set_keymap('t', '<Esc>', '<C-\\><C-n>', { noremap = true, silent = true })
+-- Allow CTRL+w to be used in terminal mode
+vim.api.nvim_set_keymap('t', '<C-w>', '<C-\\><C-n><C-w>', { noremap = true, silent = true })
 -- Move selected text down (Alt + j)
 vim.api.nvim_set_keymap('v', '<A-j>', ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
 -- Move selected text up (Alt + k)
@@ -212,11 +218,6 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
-
--- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
--- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
--- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 -- TIP: Disable arrow keys in normal mode
 -- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
